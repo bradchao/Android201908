268582cecceb910295810f4742a34a949bdfc820
brad <brad@brad.tw>
2019-12-06 19:04:04 +0800

    2

--- app/src/main/java/tw/org/iii/android201908/MainActivity.java
+++ app/src/main/java/tw/org/iii/android201908/MainActivity.java
@@ -1,7 +1,11 @@
 package tw.org.iii.android201908;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +14,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    9487);
+        }else{
+            init();
+        }
+
     }
+
+    private void init(){
+
+    }
+
 }
